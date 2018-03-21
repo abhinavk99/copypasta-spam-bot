f6018ea3116f29a33ab6be47f689ff8362150010
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-03-21 17:23:05 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# copypasta-spam-bot
+
+Reddit bot that spams random posts with copypastas
+
+Made as a joke. I don't recommend using it a lot, otherwise you might get banned by Reddit.
--- spam.py
+++ spam.py
@@ -5,10 +5,13 @@ reddit = praw.Reddit(client_id=cfg.reddit_id,
                      client_secret=cfg.reddit_secret,
                      password=cfg.reddit_pass,
                      user_agent=cfg.reddit_user_agent,
-                     username=cfg.reddit_user)
+                     username=cfg.reddit_user) 
 
+# Gets copypastas
 copypastas = []
 print('Getting copypastas')
 for post in reddit.subreddit('copypasta').top(limit=30):
-    if post.is_self:
+    if post.is_self: # Only saves the copypastas from self posts
         copypastas.append(post.selftext)
+
+# Implement spamming function later
